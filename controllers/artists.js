137340321015705d1e7f93c2e40d8af04b21142d
1RafaelSantana <rafael.que97@gmail.com>
2021-01-29 23:31:26 -0300

    buscando dados de um artista especifico

--- controllers/artists.js
+++ controllers/artists.js
@@ -1,4 +1,4 @@
-const { Artist } = require("../models")
+const { Artist, Album, MusicalGroup } = require("../models")
 
 exports.getAll = async function (req, res) {
   try {
@@ -12,3 +12,33 @@ exports.getAll = async function (req, res) {
     res.status(500).json(error);
   }
 }
+
+exports.getOne = async function (req, res) {
+  try {
+    const artistId = req.params.id;
+
+    const artist = JSON.parse(
+      JSON.stringify(
+        await Artist.findOne({
+          where: { id: artistId },
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+          include: [
+            {
+              model: Album,
+              attributes: { exclude: ["createdAt", "updatedAt"] },
+            },
+            {
+              model: MusicalGroup,
+              attributes: { exclude: ["createdAt", "updatedAt"] },
+            }
+          ]
+        })
+      )
+    );
+
+    res.json(artist);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
--- routes/artists.js
+++ routes/artists.js
@@ -5,4 +5,6 @@ const validateToken = require('../middlewares/validate_token')
 
 router.get('/artists', validateToken, controller.getAll) //rota que busca todos os artistas
 
+router.get("/artists/:id", validateToken, controller.getOne) //rota que busca os dados de um artista específico
+
 module.exports = router
